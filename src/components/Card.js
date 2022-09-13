d7bfac7d46a9f0fb9e94857ce00c3c327b73c8ca
Bakhriev <legendofpain5112@gmail.com>
2022-09-13 20:38:27 +0500

    Add React Components

---
+++ src/components/Card.js
@@ -0,0 +1,44 @@
+function Card() {
+  return (
+    <div className="card">
+      <div className="favorite">
+        <img width={32} height={32} src="/img/heart-unliked.svg" alt="heart" />
+      </div>
+      <img width={133} height={123} src="/img/sneakers/1.jpg" alt="sneakers" />
+      <p>Мужские Кроссовки Nike Blazer Mid Suede</p>
+      <div className="price-wrapper">
+        <div className="price">
+          <span>Цена:</span>
+          <b>12 999 руб.</b>
+        </div>
+        <div className="btn-wrapper">
+          <button>
+            <svg
+              width="32"
+              height="32"
+              viewBox="0 0 32 32"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <rect
+                x="0.5"
+                y="0.5"
+                width="31"
+                height="31"
+                rx="7.5"
+                fill="white"
+                stroke="#F2F2F2"
+              />
+              <path
+                d="M20.6653 15.1312H17.2021V11.6682C17.2021 10.3328 15.1311 10.3328 15.1311 11.6682V15.1312H11.668C10.3329 15.1312 10.3329 17.2022 11.668 17.2022H15.1311V20.6652C15.1311 22.0005 17.2021 22.0005 17.2021 20.6652V17.2022H20.6653C22.0005 17.2022 22.0005 15.1312 20.6653 15.1312Z"
+                fill="#D3D3D3"
+              />
+            </svg>
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Card;
---
+++ src/components/Drawer.js
@@ -0,0 +1,145 @@
+function Drawer() {
+  return (
+    <div className="drawer">
+      <div className="drawer__container">
+        <h2>Корзина</h2>
+        <div className="cartItemWrapper">
+          <div className="cartItem">
+            <img
+              width={70}
+              height={70}
+              src="/img/sneakers/1.jpg"
+              alt="sneakers"
+            />
+            <div className="cartItemText">
+              <p>Мужские Кроссовки Nike Air Max 270</p>
+              <span>12 999 руб.</span>
+            </div>
+            <img
+              className="cancel"
+              width={32}
+              height={32}
+              src="/img/cancel.svg"
+              alt="cancel"
+            />
+          </div>
+          <div className="cartItem">
+            <img
+              width={70}
+              height={70}
+              src="/img/sneakers/2.jpg"
+              alt="sneakers"
+            />
+            <div className="cartItemText">
+              <p>Мужские Кроссовки Nike Air Max 270</p>
+              <span>12 999 руб.</span>
+            </div>
+            <img
+              className="cancel"
+              width={32}
+              height={32}
+              src="/img/cancel.svg"
+              alt="cancel"
+            />
+          </div>
+          <div className="cartItem">
+            <img
+              width={70}
+              height={70}
+              src="/img/sneakers/1.jpg"
+              alt="sneakers"
+            />
+            <div className="cartItemText">
+              <p>Мужские Кроссовки Nike Air Max 270</p>
+              <span>12 999 руб.</span>
+            </div>
+            <img
+              className="cancel"
+              width={32}
+              height={32}
+              src="/img/cancel.svg"
+              alt="cancel"
+            />
+          </div>
+          <div className="cartItem">
+            <img
+              width={70}
+              height={70}
+              src="/img/sneakers/1.jpg"
+              alt="sneakers"
+            />
+            <div className="cartItemText">
+              <p>Мужские Кроссовки Nike Air Max 270</p>
+              <span>12 999 руб.</span>
+            </div>
+            <img
+              className="cancel"
+              width={32}
+              height={32}
+              src="/img/cancel.svg"
+              alt="cancel"
+            />
+          </div>
+          <div className="cartItem">
+            <img
+              width={70}
+              height={70}
+              src="/img/sneakers/1.jpg"
+              alt="sneakers"
+            />
+            <div className="cartItemText">
+              <p>Мужские Кроссовки Nike Air Max 270</p>
+              <span>12 999 руб.</span>
+            </div>
+            <img
+              className="cancel"
+              width={32}
+              height={32}
+              src="/img/cancel.svg"
+              alt="cancel"
+            />
+          </div>
+        </div>
+        <ul className="priceMenu">
+          <li>
+            <p>Итого: </p>
+            <span></span>
+            <b>21 498 руб. </b>
+          </li>
+          <li>
+            <p>Налог 5%: </p>
+            <span></span>
+            <b>1074 руб. </b>
+          </li>
+        </ul>
+        <button className="btn">
+          <p>Оформить заказ</p>
+          <svg
+            width="16"
+            height="14"
+            viewBox="0 0 16 14"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M1 7H14.7143"
+              stroke="white"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            />
+            <path
+              d="M8.71436 1L14.7144 7L8.71436 13"
+              stroke="white"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            />
+          </svg>
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default Drawer;
